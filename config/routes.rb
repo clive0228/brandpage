5b12129557c60884e2c14fb12c8bac26cb24fffa
clive0228 <clive0228@gmail.com>
2015-01-26 10:27:42 +0000

    First Timeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Brandpage::Application.routes.draw do
+  get "wall/write"
+  get "wall/posts"
 	get 'home/hello' => 'home#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
